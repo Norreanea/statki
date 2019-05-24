cbd3123469e60d62a1b1455ae2e11803101a8b7d
ELisewska <47402636+ELisewska@users.noreply.github.com>
2019-05-24 20:55:25 +0200

    Update generator.py
    nie ma "trafione zatopione" - do zrobienia. I generator losuje wiersz/kolumnę 0? powinno być od 1 do 10?

--- generator.py
+++ generator.py
@@ -32,6 +32,7 @@ class Statek():
 def generator():
         statki = 0
         while statki < 10:
+            global statki_kratki
             #lista zajmowanych przez statki kratek
             statki_kratki = []
             statki = 0
@@ -66,3 +67,53 @@ def generator():
 
 generator()
 
+punkty = [s.split(',') for s in statki_kratki]
+print(punkty)
+
+
+#####________________plansza
+plansza = []
+
+for x in range(1,11):
+    plansza.append(["o"] * 10)
+
+def print_board(plansza):
+    for row in plansza:
+        print (" ".join(row))
+
+trafienia=0
+tura=0
+
+print("Oto plansza:\n")
+print_board(plansza)
+     
+for tura in range(10):
+    zgadn_wiersz = int(input("\nZgadnij wiersz [1-10]:  "))
+    zgadn_kolumna = int(input("Zgadnij kolumnę [1-10]: "))
+    strzał=("%d_%d" %(zgadn_wiersz,zgadn_kolumna))
+
+    if (zgadn_wiersz < 0 or zgadn_wiersz > 10)  or (zgadn_kolumna < 0 or zgadn_kolumna > 10):
+       print ("Wykroczyłeś poza planszę")
+       print_board(plansza)
+    elif(plansza[zgadn_wiersz-1][zgadn_kolumna-1] == "."):
+       print ("Już tu próbowałeś. Zmarnowany ruch.")
+       print_board(plansza)
+    elif(plansza[zgadn_wiersz-1][zgadn_kolumna-1] == "X"):
+       print ("Już tu strzeliłeś statek. Zmarnowany ruch.")
+       print_board(plansza)
+
+    else:
+        for i in range(0,len(punkty)):
+         
+            if strzał==punkty[i][0]:
+                   trafienia=trafienia+1
+                   plansza[zgadn_wiersz-1][zgadn_kolumna-1] = "X"
+                   break                  
+            else:
+                   plansza[zgadn_wiersz-1][zgadn_kolumna-1] = "."
+                    
+        if plansza[zgadn_wiersz-1][zgadn_kolumna-1] == "X"   :
+               print("\nTrafione!")
+        elif plansza[zgadn_wiersz-1][zgadn_kolumna-1] == ".":
+               print("\nPudło!")
+        print_board(plansza)
