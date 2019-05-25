c921515aadb531c525fe39614632ffefce9a879f
ELisewska <47402636+ELisewska@users.noreply.github.com>
2019-05-25 19:37:43 +0200

    Update wyswietlacz.py

--- wyswietlacz.py
+++ wyswietlacz.py
@@ -2,9 +2,9 @@
 
 import sys
 import time
+global plansza
 plansza = []
-g=3
-k=5
+
 
 
 for line in sys.stdin:
@@ -20,27 +20,31 @@ def print_board(plansza):
         str(row)
         print (" ".join(row))
 
-for x in range(0,11):
-  for y in range(0,11):
-    plansza[0][x]=x
-    plansza[x][0]=x
-    plansza[0][x]=str(plansza[0][x])
-    plansza[x][0]=str(plansza[x][0])
+def make_board():
+  g=3
+  k=5
+  for x in range(0,11):
+    for y in range(0,11):
+      plansza[0][x]=x
+      plansza[x][0]=x
+      plansza[0][x]=str(plansza[0][x])
+      plansza[x][0]=str(plansza[x][0])
 
-for x in range(0,10):
-  l=[]
-  l.append(plansza[x][0])
-  l.append(" ")
-  plansza[x][0]=''.join(l)
+  for x in range(0,10):
+    l=[]
+    l.append(plansza[x][0])
+    l.append(" ")
+    plansza[x][0]=''.join(l)
 
-for i in range(0,20):
-  a=punkty[g][0][0]
-  b=punkty[k][0][0]
-  c=int(a)
-  d=int(b)
-  plansza[c][d]="X"
-  g+=9
-  k+=9
+  for i in range(0,20):
+    a=punkty[g][0][0]
+    b=punkty[k][0][0]
+    c=int(a)
+    d=int(b)
+    plansza[c][d]="X"
+    g+=9
+    k+=9
 
+make_board()
 print("Oto plansza:\n")
 print_board(plansza)
